43f9dec91c29adea73ff0ea8266fc2dee6172466
Kevin Ullyott <Orrison@users.noreply.github.com>
2017-07-13 12:25:33 -0700

    Create drsInstagram.js

---
+++ drsInstagram.js
@@ -0,0 +1,20 @@
+<script>
+(function($) {
+
+var ua = navigator.userAgent.toLowerCase(); // Assigns the User Agent to a variable and makes it lowercase
+var isIphone = ua.indexOf("iphone") > -1; // Checks if iPhone
+var isIpad = ua.indexOf("ipad") > -1; // Checks if iPad
+var isAndroid = ua.indexOf("android") > -1; // Checks if Android, will work on some Windows mobiles
+
+if(isIphone || isIpad) {
+  setTimeout(function () { window.location = "https://www.instagram.com/rentyrentals/"; }, 5000); // Fallback in the event native app opening fails
+  window.location = "instagram://user?username=[INSTAGRAM USERNAME HERE]"; // Opens IG link in iOS native app
+} else if (isAndroid) {
+  setTimeout(function () { window.location = "https://www.instagram.com/rentyrentals"; }, 25); // Fallback in the event native app opening fails
+  window.location = "intent://instagram.com/_u/[INSTAGRAM USERNAME HERE]/#Intent;package=com.instagram.android;scheme=https;end"; // Opens IG link in Android native app
+} else {
+  window.location = "https://www.instagram.com/rentyrentals/"; // Defaults to opening link in the browser
+}
+
+}) ( jQuery );
+</script>
