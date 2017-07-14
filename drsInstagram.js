64a0d2c1139d5a4a33a42ae925f71cac5afa6533
Kevin Ullyott <Orrison@users.noreply.github.com>
2017-07-13 19:43:37 -0700

    Update drsInstagram.js

--- drsInstagram.js
+++ drsInstagram.js
@@ -7,13 +7,13 @@ var isIpad = ua.indexOf("ipad") > -1; // Checks if iPad
 var isAndroid = ua.indexOf("android") > -1; // Checks if Android, will work on some Windows mobiles
 
 if(isIphone || isIpad) {
-  setTimeout(function () { window.location = "https://www.instagram.com/rentyrentals/"; }, 5000); // Fallback in the event native app opening fails
+  setTimeout(function () { window.location = "https://www.instagram.com/[INSTAGRAM USERNAME HERE]/"; }, 5000); // Fallback in the event native app opening fails
   window.location = "instagram://user?username=[INSTAGRAM USERNAME HERE]"; // Opens IG link in iOS native app
 } else if (isAndroid) {
-  setTimeout(function () { window.location = "https://www.instagram.com/rentyrentals"; }, 25); // Fallback in the event native app opening fails
+  setTimeout(function () { window.location = "https://www.instagram.com/[INSTAGRAM USERNAME HERE]/"; }, 25); // Fallback in the event native app opening fails
   window.location = "intent://instagram.com/_u/[INSTAGRAM USERNAME HERE]/#Intent;package=com.instagram.android;scheme=https;end"; // Opens IG link in Android native app
 } else {
-  window.location = "https://www.instagram.com/rentyrentals/"; // Defaults to opening link in the browser
+  window.location = "https://www.instagram.com/[INSTAGRAM USERNAME HERE]/"; // Defaults to opening link in the browser
 }
 
 }) ( jQuery );
