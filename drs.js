dc22a4dc2ee80097cfbc6899cf7877d5593c50d9
Kevin Ullyott <Orrison@users.noreply.github.com>
2017-10-19 03:55:18 -0700

    Fixed undefined variables

--- drs.js
+++ drs.js
@@ -6,68 +6,68 @@ var isIphone = ua.indexOf("iphone") > -1; // Checks if iPhone
 var isIpad = ua.indexOf("ipad") > -1; // Checks if iPad
 var isAndroid = ua.indexOf("android") > -1; // Checks if Android, will work on some Windows mobiles
 
-$fbUserID = 423992034621484;
+var fbUserID = 423992034621484;
     
 if (isIphone || isIpad) {
     $('a[href*="facebook.com"]').on('click', function(e){
-        e.preventDefault;
-        $fUser = $('a[href*="facebook.com"]').attr("href").match(/facebook.com\/([a-zA-Z_.-]+)/)[1];
+        e.preventDefault();
+        var fUser = $('a[href*="facebook.com"]').attr("href").match(/facebook.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
         setTimeout(function () {
         if (new Date().valueOf() - now > 100) return;
-            window.location = "https://www.facebook.com/" + $fUser;
+            window.location = "https://www.facebook.com/" + fUser;
         }, 50);
-        window.location.href = "fb://profile/" + $fbUserID; // Opens FB link in iOS native app
+        window.location.href = "fb://profile/" + fbUserID; // Opens FB link in iOS native app
     });
     $('a[href*="instagram.com"]').on('click', function(e){
-        e.preventDefault;
-        $iUser = $(this).attr("href").match(/instagram.com\/([a-zA-Z_.-]+)/)[1];
+        e.preventDefault();
+        var iUser = $(this).attr("href").match(/instagram.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
         setTimeout(function () {
         if (new Date().valueOf() - now > 100) return;
-            window.location = "https://www.instagram.com/" + $iUser;
+            window.location = "https://www.instagram.com/" + iUser;
         }, 50);
-        window.location = "instagram://user?username=" + $iUser; // Opens IG link in iOS native app
+        window.location = "instagram://user?username=" + iUser; // Opens IG link in iOS native app
     });
     $('a[href*="twitter.com"]').on('click', function(e){
-        e.preventDefault;
-        $tUser = $(this).attr("href").match(/twitter.com\/([a-zA-Z_.-]+)/)[1];
+        e.preventDefault();
+        var tUser = $(this).attr("href").match(/twitter.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
         setTimeout(function () {
         if (new Date().valueOf() - now > 100) return;
-            window.location = "https://www.twitter.com/" + $tUser;
+            window.location = "https://www.twitter.com/" + tUser;
         }, 50);
-        window.location = "twitter://user?screen_name=" + $tUser; // Opens IG link in iOS native app
+        window.location = "twitter://user?screen_name=" + tUser; // Opens IG link in iOS native app
     });
 } else if (isAndroid) {
     $('a[href*="facebook.com"]').on('click', function(e){
         e.preventDefault();
-        $fUser = $(this).attr("href").match(/facebook.com\/([a-zA-Z_.-]+)/)[1];
+        var fUser = $(this).attr("href").match(/facebook.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
         setTimeout(function () {
         if (new Date().valueOf() - now > 100) return;
-            window.location = "https://www.facebook.com/" + $fUser;
+            window.location = "https://www.facebook.com/" + fUser;
         }, 50);
-        window.location.href = "fb://page/" + $fbUserID; // Opens FB link in Android native app
+        window.location.href = "fb://page/" + fbUserID; // Opens FB link in Android native app
     });
     $('a[href*="instagram.com"]').on('click', function(e){
         e.preventDefault();
-        $iUser = $(this).attr("href").match(/instagram.com\/([a-zA-Z_.-]+)/)[1];
+        var iUser = $(this).attr("href").match(/instagram.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
         setTimeout(function () {
         if (new Date().valueOf() - now > 100) return;
-            window.location = "https://www.instagram.com/" + $iUser;
+            window.location = "https://www.instagram.com/" + iUser;
         }, 50);
-        window.location.href = "intent://instagram.com/_u/" + $iUser + "/#Intent;package=com.instagram.android;scheme=https;end"; // Opens IG link in Android native app
+        window.location.href = "intent://instagram.com/_u/" + iUser + "/#Intent;package=com.instagram.android;scheme=https;end"; // Opens IG link in Android native app
     });
     $('a[href*="twitter.com"]').on('click', function(e){
-        $tUser = $(this).attr("href").match(/twitter.com\/([a-zA-Z_.-]+)/)[1];
+        var tUser = $(this).attr("href").match(/twitter.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
         setTimeout(function () {
         if (new Date().valueOf() - now > 100) return;
-            window.location = "https://www.twitter.com/" + $tUser;
+            window.location = "https://www.twitter.com/" + tUser;
         }, 500);
-        window.location = "twitter://user?screen_name=" + $tUser; // Opens Twitter link in Android native app
+        window.location = "twitter://user?screen_name=" + tUser; // Opens Twitter link in Android native app
     });
 }
 
