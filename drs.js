cd884d63b86824b0982efbab7e1d009ad15850ae
Kevin Ullyott <ullyott.kevin@gmail.com>
2018-10-17 09:20:56 -0700

    Converted jquery on clicks to click function

--- drs.js
+++ drs.js
@@ -9,7 +9,7 @@ var isAndroid = ua.indexOf("android") > -1; // Checks if Android, will work on s
 var fbUserID = 423992034621484;
     
 if (isIphone || isIpad) {
-    $('a[href*="facebook.com"]').on('click', function(e){
+    $('a[href*="facebook.com"]').click(function(e){
         e.preventDefault();
         var fUser = $(this).attr("href").match(/facebook\.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
@@ -19,7 +19,7 @@ if (isIphone || isIpad) {
         }, 50);
         window.location.href = "fb://profile/" + fbUserID; // Opens FB link in iOS native app
     });
-    $('a[href*="instagram.com"]').on('click', function(e){
+    $('a[href*="instagram.com"]').click(function(e){
         e.preventDefault();
         var iUser = $(this).attr("href").match(/instagram\.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
@@ -29,7 +29,7 @@ if (isIphone || isIpad) {
         }, 50);
         window.location = "instagram://user?username=" + iUser; // Opens IG link in iOS native app
     });
-    $('a[href*="twitter.com"]').on('click', function(e){
+    $('a[href*="twitter.com"]').click(function(e){
         e.preventDefault();
         var tUser = $(this).attr("href").match(/twitter\.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
@@ -40,7 +40,7 @@ if (isIphone || isIpad) {
         window.location = "twitter://user?screen_name=" + tUser; // Opens IG link in iOS native app
     });
 } else if (isAndroid) {
-    $('a[href*="facebook.com"]').on('click', function(e){
+    $('a[href*="facebook.com"]').click(function(e){
         e.preventDefault();
         var fUser = $(this).attr("href").match(/facebook\.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
@@ -50,7 +50,7 @@ if (isIphone || isIpad) {
         }, 50);
         window.location.href = "fb://page/" + fbUserID; // Opens FB link in Android native app
     });
-    $('a[href*="instagram.com"]').on('click', function(e){
+    $('a[href*="instagram.com"]').click(function(e){
         e.preventDefault();
         var iUser = $(this).attr("href").match(/instagram\.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
@@ -60,7 +60,7 @@ if (isIphone || isIpad) {
         }, 50);
         window.location.href = "intent://instagram.com/_u/" + iUser + "/#Intent;package=com.instagram.android;scheme=https;end"; // Opens IG link in Android native app
     });
-    $('a[href*="twitter.com"]').on('click', function(e){
+    $('a[href*="twitter.com"]').click(function(e){
         var tUser = $(this).attr("href").match(/twitter\.com\/([a-zA-Z_.-]+)/)[1];
         var now = new Date().valueOf();
         setTimeout(function () {
